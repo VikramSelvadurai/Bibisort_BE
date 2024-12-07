e482866b671872accd54ea75f5e5bb7da76d87a5
vikram <vikram.s@platform3solutions.com>
2024-12-07 18:38:01 +0530

    initial commit

---
+++ src/main/java/com/example/bigbisort_be/BigbisortBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigbisort_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbisortBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigbisortBeApplication.class, args);
+    }
+
+}
